c93eb8fe960b75c405ad6a4c9d6b5a955a253f3b
Takayuki Kaburagi <t.kaburagi@me.com>
2019-11-09 18:39:10 +0900

    refactored

--- face-bootifier/src/main/java/am/ik/demo/facebootifier/SlackClient.java
+++ face-bootifier/src/main/java/am/ik/demo/facebootifier/SlackClient.java
@@ -2,9 +2,6 @@ package am.ik.demo.facebootifier;
 
 import com.ullink.slack.simpleslackapi.SlackSession;
 import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
